f133e94b3d5de1abea041fe2567e7b0f4c9e9c0b
Aaron Bruce <aarondbruce@gmail.com>
2012-05-21 09:41:31 -0700

    Copied the instructions from the aws sdk config file into the module's config.

--- config/aws.php
+++ config/aws.php
@@ -1,5 +1,38 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
+//NOTE: These instructsions were copied out of the example config that is packaged with the sdk.
+/*###################################################################################################
+
+    As of version 1.5, the AWS SDK for PHP uses the CFCredentials class to handle credentials.
+	This class has the advantage of being able to support multiple sets of credentials at a time,
+	including the ability for credential sets to inherit settings from other credential sets.
+
+	Some example uses are noted at https://gist.github.com/1478912
+
+	Notes:
+
+	* You can define one or more credential sets.
+
+	* Credential sets can be named anything that PHP allows for an associative array key;
+	  "production", "staging", etc., are just sample values. Feel free to rename them.
+
+	* A credential set only has four required entries: key, secret, default_cache_config and
+	  certificate_authority. Aside from these, you can add any additional bits of information
+	  you'd like to keep easily accessible (e.g., multi-factor authentication device key, your
+	  AWS Account ID, your canonical identifiers).
+
+	* Additional credential sets can inherit the properties of another credential set using the
+	  @inherit keyword.
+
+	* If more than one credential set is provided, a default credential set must be specified
+	  using the @default keyword.
+
+	* If you only have one credential set, you can set it to the @default keyword.
+
+	* View the documentation for the CFCredentials::set() method to view usage examples.
+
+###################################################################################################*/
+
 return array(
 
     // Credentials for the development environment.
